7ee38b4124c2cc5316c2c67f6e2a59cba91eda7b
Cameron Taaffe <cameron.taaffe@gmail.com>
2020-09-29 23:16:05 -0400

    Problems from chapter 2 finished.

--- afile_server.erl
+++ afile_server.erl
@@ -3,13 +3,17 @@
 
 start(Dir) -> spawn(afile_server, loop, [Dir]).
 
-loop(Dir) ->
-	receive
-		{Client, list_dir} ->
-			Client ! {self(), file:list_dir(Dir)};
-		{Client, {get_file, File}} ->
-			Full = filename:join(Dir, File),
-			Client ! {self(), file:read_file(Full)}
-	end,
-	loop(Dir).
+full_path(Dir, File) -> filename:join(Dir, File).
 
+loop(Dir) ->
+  receive
+    {Client, list_dir} ->
+      Client ! {self(), file:list_dir(Dir)};
+    {Client, {get_file, File}} ->
+      Full = full_path(Dir, File),
+      Client ! {self(), file:read_file(Full)};
+    {Client, {put_file, File, Message}} ->
+      Full = full_path(Dir, File),
+      Client ! {self(), file:write_file(Full, Message)}
+  end,
+  loop(Dir).
