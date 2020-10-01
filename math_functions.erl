8c7c12a618c52d777dc4b663571b9ee8860393f7
Cameron Taaffe <cameron.taaffe@gmail.com>
2020-10-01 16:15:08 -0400

    Chapter 4 problem 7 done. Chapter 4 complete.

--- math_functions.erl
+++ math_functions.erl
@@ -1,5 +1,5 @@
 -module(math_functions).
--export([even/1, odd/1, filter/2, test/0]).
+-export([even/1, odd/1, filter/2, split/1, test/0]).
 
 test() ->
 	true = even(2),
@@ -8,9 +8,12 @@ test() ->
 	false = odd(12),
 	[2, 4, 6] = filter(fun even/1, [1, 2, 3, 4, 5, 6]),
 	[1, 3, 5] = filter(fun odd/1, [1, 2, 3, 4, 5, 6]),
+	{[1, 3, 5], [2, 4, 6]} = split([1, 2, 3, 4, 5, 6]),
 	test_worked.
 
 even(X) -> X rem 2 =:= 0.
 odd(X) -> X rem 2 =:= 1.
 
 filter(F, L) -> [X || X <- L, F(X)].
+
+split(L) -> {filter(fun odd/1, L), filter(fun even/1, L)}.
